d838f81e4bcf0ce4566ec9031743c6e67e2ca645
Harrison Rose <sevenlayercookie@gmail.com>
2022-05-19 21:36:43 -0500

    formatted

--- javascript.js
+++ javascript.js
@@ -39,11 +39,11 @@ function onload() {
     flatline = 0,
     px = 0,
     opx = 0,
-    speed = .3, // speed of the cursor across the screen; affects spacing of data
+    speed = 0.3, // speed of the cursor across the screen; affects spacing of data
     processSpeed = 2; //skips data points to "speed" the drawing; less resolution
-    isPainted = true;
-    timestamp = performance.now();
-    paintCount = 1;
+  isPainted = true;
+  timestamp = performance.now();
+  paintCount = 1;
   (py = h * 1), (opy = py), (scanBarWidth = 40), (PVCflag = 0);
   k = 0;
   ctx.strokeStyle = "#00bd00";
@@ -59,13 +59,11 @@ function onload() {
   var j = 0;
   var jold = 0;
 
- 
   // initiate animation (is looped)
   function startAnimating() {
     ctx.beginPath();
     for (let i = 0; i < 1200 && isPainted; i++) {
       loop();
-      
     }
   }
 
@@ -74,14 +72,12 @@ function onload() {
 
   var lold = 0;
   function updateHertz() {
-
     sec++;
     avgFPS = document.getElementById("demoTEXT").innerText = l - lold;
     document.getElementById("demoTEXT3").innerText = processingSpeed = j - jold;
     lold = l;
     jold = j;
   }
-
 
   function parseData() {
     py = -parseInt(data[i] * 1000) / 8 + verticalPositionFactor;
@@ -94,31 +90,26 @@ function onload() {
   function loop() {
     l++; //count # of times through loop
     ctx.beginPath();
-    for (let z = 0; z < dataHertz/avgFPS; z++) 
-    {
+    for (let z = 0; z < dataHertz / avgFPS; z++) {
       parseData();
       px += speed; // horizontal pixels per data point
       ctx.moveTo(opx, opy);
       ctx.lineTo(px, py);
       opx = px;
       opy = py;
-      
-      if (opx > w) 
-        {
+
+      if (opx > w) {
         px = opx = 0; //-speed;
-        ctx.clearRect(0,0,canvas.width,canvas.height);
-        } 
-  
-      
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+      }
     }
-    isPainted=false;
+    isPainted = false;
     requestAnimationFrame(paint);
     //document.getElementById('demoTEXT').innerText = PVCflag;
     document.getElementById("demoTEXT2").innerText = i;
   }
 
-  function paint()
-  {
+  function paint() {
     ctx.stroke();
     isPainted = true;
     startAnimating();
