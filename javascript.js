994ba6c8b2006150487467734add10d12d9779b9
Harrison Rose <sevenlayercookie@gmail.com>
2022-05-19 21:34:21 -0500

    optimized by browser framerate!

--- javascript.js
+++ javascript.js
@@ -8,6 +8,16 @@
     - painter should run at browser framerate (max for device) for smoothness
     - data processor should run as fast as possible, but 
 
+    width = 1000 pixels
+
+    60  bpm = 1 beat / 1000 msec
+
+    R-R should take one second to paint
+      - # data points per R-R = 500 Hz * 1 second = 500 data points
+
+    so 500 data points should be processed every second
+    paint will paint at Max Browser Framerate. So data processing should be 500/Max Browse Frame Rate and then paint 
+
 */
 
 function onload() {
@@ -19,8 +29,8 @@ function onload() {
     sec = 0,
     avgRefresh = 1,
     dataHertz = 500, // in Hz (data points per second)
-    animatefps = 30,
-    animateRatio = dataHertz / animatefps,
+    avgFPS = 144,
+    animateRatio = dataHertz / avgFPS,
     dataVoltage = 10, // in mV
     compressHfactor = 1,
     heartrate = 60,
@@ -29,8 +39,11 @@ function onload() {
     flatline = 0,
     px = 0,
     opx = 0,
-    speed = 1, // speed of the cursor across the screen; affects spacing of data
+    speed = .3, // speed of the cursor across the screen; affects spacing of data
     processSpeed = 2; //skips data points to "speed" the drawing; less resolution
+    isPainted = true;
+    timestamp = performance.now();
+    paintCount = 1;
   (py = h * 1), (opy = py), (scanBarWidth = 40), (PVCflag = 0);
   k = 0;
   ctx.strokeStyle = "#00bd00";
@@ -44,6 +57,31 @@ function onload() {
   var p = 0;
   var i = 0;
   var j = 0;
+  var jold = 0;
+
+ 
+  // initiate animation (is looped)
+  function startAnimating() {
+    ctx.beginPath();
+    for (let i = 0; i < 1200 && isPainted; i++) {
+      loop();
+      
+    }
+  }
+
+  setInterval(updateHertz, 1000);
+  startAnimating();
+
+  var lold = 0;
+  function updateHertz() {
+
+    sec++;
+    avgFPS = document.getElementById("demoTEXT").innerText = l - lold;
+    document.getElementById("demoTEXT3").innerText = processingSpeed = j - jold;
+    lold = l;
+    jold = j;
+  }
+
 
   function parseData() {
     py = -parseInt(data[i] * 1000) / 8 + verticalPositionFactor;
@@ -53,44 +91,28 @@ function onload() {
     if (i >= data.length) i = 0;
   }
 
-  // initiate animation (is only run once)
-  function startAnimating() {
-    ctx.beginPath();
-    for (let i = 0; i < 1400; i++) {
-      setInterval(loop(), 2);
-    }
-  }
-
-  setInterval(updateHertz, 1000);
-  startAnimating();
-
-  var lold = 0;
-  function updateHertz() {
-    sec++;
-    avgRefresh = document.getElementById("demoTEXT").innerText = l - lold;
-    lold = l;
-  }
-
   function loop() {
-    // request another frame
-    parseData()
     l++; //count # of times through loop
-    px += speed; // horizontal pixels per data point
-    //ctx.clearRect(px, 0, scanBarWidth, h);
     ctx.beginPath();
-    ctx.moveTo(opx, opy);
-    ctx.lineTo(px, py);
-    if (i%10==0)
+    for (let z = 0; z < dataHertz/avgFPS; z++) 
     {
-    requestAnimationFrame(paint)
+      parseData();
+      px += speed; // horizontal pixels per data point
+      ctx.moveTo(opx, opy);
+      ctx.lineTo(px, py);
+      opx = px;
+      opy = py;
+      
+      if (opx > w) 
+        {
+        px = opx = 0; //-speed;
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+        } 
+  
+      
     }
-    
-    opx = px;
-    opy = py;
-
-    if (opx > w) {
-      px = opx = 0; //-speed;
-    }
+    isPainted=false;
+    requestAnimationFrame(paint);
     //document.getElementById('demoTEXT').innerText = PVCflag;
     document.getElementById("demoTEXT2").innerText = i;
   }
@@ -98,6 +120,9 @@ function onload() {
   function paint()
   {
     ctx.stroke();
+    isPainted = true;
+    startAnimating();
+    paintCount++;
   }
 
   document.getElementById("demoTEXT").onkeydown = function () {
